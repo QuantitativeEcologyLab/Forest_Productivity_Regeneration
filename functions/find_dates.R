a5140b3302163d615681df9bb5c4e08b4e479a40
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2023-09-19 22:18:32 -0600

    created fcn to calculate date since last fire/cut

---
+++ functions/find_dates.R
@@ -0,0 +1,41 @@
+library('dplyr')     # for data wrangling
+library('purrr')     # for functional programming
+library('sf')        # for spatial data
+library('terra')     # for working with rasters
+
+find_dates <- function(.date, shp, mask) {
+  s <- filter(shp, date <= .date) # only keep fires prior to .date
+  
+  .raster <-
+    map(1:nrow(s), \(.i) { # for each fire
+      .r <- slice(s, .i) %>% # only take the last fire
+        rasterize(r, cover = TRUE, background = 0) # for this fire only
+      
+      values(.r) <- if_else(values(.r) > 0, # change burned areas to date
+                            lubridate::decimal_date(s[.i, ]$date),
+                            0)
+      return(.r)
+    }) %>%
+    rast() %>% # turn list into raster stack
+    max() %>% # take the latest burn date for each cell
+    mask(mask) # drop areas outside study area shapefile
+  
+  names(.raster) <- .date # specify the reference date as the layer name
+  return(.raster)
+}
+
+# test the function
+if(FALSE) {
+  r <- rast('data/ndvi-rasters/study-area-albers/VI_16Days_250m_v61/NDVI/MOD13Q1_NDVI_2000_049.tif')
+  values(r) <- 1
+  
+  fires <- st_read('data/fires/study-fires.shp')
+  area <- st_read('data/study-area/study-area-albers.shp')
+  
+  plot(c(find_dates(as.Date('1925-01-01'), shp = fires, mask = area),
+         find_dates(as.Date('1926-01-01'), shp = fires, mask = area),
+         find_dates(as.Date('1927-01-01'), shp = fires, mask = area),
+         find_dates(as.Date('1928-01-01'), shp = fires, mask = area),
+         find_dates(as.Date('1929-01-01'), shp = fires, mask = area),
+         find_dates(as.Date('1930-01-01'), shp = fires, mask = area)))
+}
