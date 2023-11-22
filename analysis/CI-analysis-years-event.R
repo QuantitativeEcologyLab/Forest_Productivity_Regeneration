8883ea5060d9da4689b85f57b90b0f974db225d5
danidevries <145375705+danidevries@users.noreply.github.com>
2023-11-22 11:36:27 -0800

    Update CI-analysis-years-event.R

--- analysis/CI-analysis-years-event.R
+++ analysis/CI-analysis-years-event.R
@@ -6,31 +6,31 @@ source('analysis/ggplot_theme.R')
 d <- readRDS('data/labelled-ndvi-data.rds')
 m <- readRDS('models/betals-gamls-2023-11-08.rds')
 
-newd_year<- expand_grid(doy = 0,
-                       year = 2000:2023,
-                       x_alb = 0,
-                       y_alb = 0,
-                       event = c('0', 'f', 'c'),
-                       years_since=0:103)
-
-preds_year <- bind_cols(newd_year,
-            predict(m, newdata = newd_year,
-                    terms = c('s(year)', 's(year,event)', 's.1(year)','s.1(year,event)')))
-
-
-#CI of the mean using Stefanos function for CIof betals gamls 
-mean_year<-betals_mean(m, preds_year, nsims=1e4)%>%
+mean_year<-betals_mean(m, d, nsims=1e4)%>%
   group_by(year,event)%>%
+  expand_grid(doy = 0,
+              year = 2000:2023,
+              x_alb = 0,
+              y_alb = 0,
+              event = c('0', 'f', 'c'),
+              years_since=0)%>%
   summarize(mu = median(mean),
             lwr.mu = quantile(mean, probs = 0.025),
             upr.mu = quantile(mean, probs = 0.92))#avg between 89 and 95
 
 
-variance_year <- betals_var(m,preds_year, nsims = 1e4) %>%
+variance_year <- betals_var(m,d, nsims = 1e4) %>%
   group_by(year, event) %>%
+  expand_grid(doy = 0,
+              year = 2000:2023,
+              x_alb = 0,
+              y_alb = 0,
+              event = c('0', 'f', 'c'),
+              years_since=0)%>%
   summarize(variance_year = median(variance),
             lwr.s2 = quantile(variance, probs = 0.025),
             upr.s2 = quantile(variance, probs = 0.92))
+
 
  g1<- mean_year%>%
    ggplot(aes(x=year, y= mu, color= event))+
