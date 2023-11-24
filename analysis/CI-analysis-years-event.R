62d4d591d98836c52a138ce76486ea57f41f7958
danidevries <145375705+danidevries@users.noreply.github.com>
2023-11-24 12:53:03 -0800

    Update CI-analysis-years-event.R

--- analysis/CI-analysis-years-event.R
+++ analysis/CI-analysis-years-event.R
@@ -6,36 +6,29 @@ source('analysis/ggplot_theme.R')
 d <- readRDS('data/labelled-ndvi-data.rds')
 m <- readRDS('models/betals-gamls-2023-11-08.rds')
 
-mean_year<-betals_mean(m, d, nsims=1e4)%>%
-  group_by(year,event)%>%
-  expand_grid(doy = 0,
-              year = 2000:2023,
-              x_alb = 0,
-              y_alb = 0,
-              event = c('0', 'f', 'c'),
-              years_since=0)%>%
+#create a new dataset for the function (decrease computation) and filters appropriately
+newd_year<-expand_grid(date=0,#expand_grid returns a tibble whereas expand.grid returns a df
+                       year= 2000:2023,
+                       doy= 0,
+                       ndvi=0,
+                       event = unique(d$event),
+                       years_since=0,
+                       x_alb=0,
+                       y_alb=0)
+
+mean_year<-betals_mean(m, newd_year, nsims=1e4, unconditional  = FALSE, terms= c('s(year)', 's(year,event)', 's.1(year)', 's.1(year,event)'))%>%
+  group_by(year,event)#%>%
   summarize(mu = median(mean),
             lwr.mu = quantile(mean, probs = 0.025),
-            upr.mu = quantile(mean, probs = 0.92))#avg between 89 and 95
+            upr.mu = quantile(mean, probs = 0.975))
 
 
-variance_year <- betals_var(m,d, nsims = 1e4) %>%
+
+variance_year <- betals_var(m,newd_year, nsims = 1e4, unconditional  = FALSE, terms = c('s(year)', 's(year,event)','s.1(year)', 's.1(year,event)')) %>%
   group_by(year, event) %>%
-  expand_grid(doy = 0,
-              year = 2000:2023,
-              x_alb = 0,
-              y_alb = 0,
-              event = c('0', 'f', 'c'),
-              years_since=0)%>%
   summarize(variance_year = median(variance),
             lwr.s2 = quantile(variance, probs = 0.025),
-            upr.s2 = quantile(variance, probs = 0.92))
+            upr.s2 = quantile(variance, probs = 0.975))
 
-
- g1<- mean_year%>%
-   ggplot(aes(x=year, y= mu, color= event))+
-   geom_ribbon(aes(y=lwr.mu, fill=event) )+
-   geom_ribbon(aes(y=upr.mu, fill = event))
-plot(g1)   
 
 
