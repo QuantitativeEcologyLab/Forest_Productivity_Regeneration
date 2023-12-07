566bdd5aef9cb1a51dd25b17805b2048981254f5
danidevries <145375705+danidevries@users.noreply.github.com>
2023-12-07 13:50:08 -0800

    Update CI-analysis-years-event.R

--- analysis/CI-analysis-years-event.R
+++ analysis/CI-analysis-years-event.R
@@ -1,5 +1,11 @@
 #Credible intervals for betals-gamls
-library(ggplot2)
+library('dplyr')   # for data wrangling
+library('tidyr')   # for data wrangling
+library('ggplot2') # for fancy plots
+library('mgcv')    # for GAMs
+library('gratia')  # for visualizing GAMs
+library('tidyr')
+library('cowplot')
 source('functions/betals-variance-sims-and-derivatives.R')
 source('analysis/ggplot_theme.R')
 #import data and most recent model
@@ -8,7 +14,7 @@ m <- readRDS('models/betals-gamls-2023-11-08.rds')
 
 #create a new dataset for the function (decrease computation) and filters appropriately
 newd_year<-expand_grid(date=0,#expand_grid returns a tibble whereas expand.grid returns a df
-                       year= 2000:2023,
+                       year= seq(from = 2000, to= 2023, by = 0.01),
                        doy= 0,
                        ndvi=0,
                        event = unique(d$event),
@@ -16,38 +22,46 @@ newd_year<-expand_grid(date=0,#expand_grid returns a tibble whereas expand.grid 
                        x_alb=0,
                        y_alb=0)
 
+
 mean_year<-betals_mean(m, newd_year, nsims=1e4, unconditional  = FALSE, terms= c('s(year)', 's(year,event)', 's.1(year)', 's.1(year,event)'))%>%
-  group_by(year,event)%>%
+  group_by(year, event)%>%
   summarize(mu = median(mean),
             lwr.mu = quantile(mean, probs = 0.025),
-            upr.mu = quantile(mean, probs = 0.975))
+            upr.mu = quantile(mean, probs = 0.975))%>%
+  mutate(across(c(mu, upr.mu, lwr.mu), function(x)x*2-1))#back transforming the NDVI
 
-
+ 
 
 variance_year <- betals_var(m,newd_year, nsims = 1e4, unconditional  = FALSE, terms = c('s(year)', 's(year,event)','s.1(year)', 's.1(year,event)')) %>%
   group_by(year, event) %>%
   summarize(variance_year = median(variance),
             lwr.s2 = quantile(variance, probs = 0.025),
-            upr.s2 = quantile(variance, probs = 0.975))
+            upr.s2 = quantile(variance, probs = 0.975))%>%
+  mutate(across(c(variance_year, upr.s2, lwr.s2), function(y)y * 4))#back transforming the NDVI
 
+pal <- c("#000000", "#EE6677", "#228833")#creating a color palette
 
 mean_yearplot<-ggplot(mean_year)+
-  geom_line(aes(year, mu, color = event), mean_year, alpha = 0.2, linewidth = 1)+
-  geom_ribbon(aes(year, ymin = lwr.mu, ymax = upr.mu,
-                                   fill = event), alpha = 0.3)+
-  labs(x = 'Year', y = expression(Mean~NDVI~(mu))) +
-  scale_color_bright(name = 'Event', labels = c('Control', 'Cut',
-                                                'Burned'))+
+  default+
+  geom_ribbon(aes(year, ymin = lwr.mu, ymax = upr.mu, fill = event), alpha = 0.3)+
+  geom_line(aes(year, mu, color = event), mean_year, linewidth = 1)+
+  labs(x = 'Year', y = 'Mean NDVI (\U03BC)') +
+  scale_fill_manual(values = pal, name = "Event", labels= c('Cut', 'Burned', 'Control'), aesthetics = c('color', 'fill'))+
+  scale_color_manual(values = pal, name = "Event", labels= c('Cut', 'Burned', 'Control'), aesthetics = c('color', 'fill'))+
   theme(legend.position="none")
+                     
 
+plot(mean_yearplot)
 variance_yearplot<-ggplot(variance_year)+
-  geom_line(aes(year, variance_year, color = event), variance_year, alpha = 0.2, linewidth = 1)+
-  geom_ribbon(aes(year, ymin = lwr.s2, ymax = upr.s2,
-                  fill = event), alpha = 0.3)+
-  labs(x = 'Year', y = expression(Variance~'in'~'NDVI,'~sigma^2)) +
-  scale_color_bright(name = 'Event', labels = c('Control', 'Cut',
-                                                'Burned'))+
+  default+
+  geom_ribbon(aes(year, ymin = lwr.s2, ymax = upr.s2,fill = event), alpha = 0.3)+
+  geom_line(aes(year, variance_year, color = event), variance_year, linewidth = 1)+
+  labs(x = 'Year', y = 'Variance in NDVI (\U03C3)^2') +
+  scale_fill_manual(values = pal, name = "Event", labels= c('Cut', 'Burned', 'Control'), aesthetics = c('color', 'fill'))+
+  scale_color_manual(values = pal, name = "Event", labels= c('Cut', 'Burned', 'Control'), aesthetics = c('color', 'fill'))+
   theme(legend.position="none")
+ 
+
 plot(variance_yearplot)
 
 plot_grid(
