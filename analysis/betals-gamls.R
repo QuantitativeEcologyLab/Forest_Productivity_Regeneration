ef18f9fc1c9a659596192adb6db77e55ae87862b
danidevries <145375705+danidevries@users.noreply.github.com>
2023-12-12 11:04:32 -0800

    Update betals-gamls.R
    updated smooths, filtering the burned area to control beyond 50yrs. This one is running w an errorcode

--- analysis/betals-gamls.R
+++ analysis/betals-gamls.R
@@ -1,25 +1,41 @@
 library('dplyr') # for data wrangling
 library('mgcv')  # for GAMs
-source('../ndvi-stochasticity/functions/betals.r')
+library('ggplot2')
+library('tictoc')
+source('C:/Users/danidv.stu/OneDrive - UBC/Directed-Study-main/functions/betals.r')
 
 d <- readRDS('data/labelled-ndvi-data.rds')
 
+cut_off<-ggplot(d, aes(years_since, fill = event)) +
+  geom_density(alpha = 0.3) +
+  coord_cartesian(ylim = c(0, 0.15)) +
+  geom_vline(xintercept = 50, lty = 'dashed')
+
+# assume that anything past 50 years since the event has regenerated
+d <- mutate(d,
+            event = if_else(years_since > 50, '0', event),
+            years_since = if_else(years_since > 50, 0, years_since))
+
+d1<- sample_n(d, 1e6)
+#model currently running with error code "Error in names(dat) <- object$term : 
+#  'names' attribute [2] must be the same length as the vector [1]"
+tic()
 #' cannot use `bam()` with location-scale families (yet...)
 m <- gam(formula = list(
   # linear predictor for the mean (mu)
   ndvi_scaled ~
     ## global changes
     # change of ndvi over space
-    s(x_alb, y_alb, bs = 'ds', k = 200) +
+    s(x_alb, y_alb, bs = 'ds', k = 50) +
     # seasonal change in ndvi
-    s(doy, bs = 'cc', k = 10) +
+   #s(doy, bs = 'cc', k = 10) +
     # yearly change in ndvi
-    s(year, bs = 'cr', k = 20) + # cr rather of tp because nrow(d) is high 
+    s(year, bs = 'cr', k = 10) + # cr rather of tp because nrow(d) is high 
     # change in seasonal trend over the years
     ti(doy, year, bs = c('cc', 'cr'), k = c(5, 10)) +
     ## treatment-level changes (intercept are included in smooths)
     # trees affect snow melt over doy
-    s(doy, event, bs = 'sz', k = 5, xt = list(bs = 'cc')) +
+    s(doy, event, bs = 'fs', k = 10, xt = list(bs = 'cc')) +
     # trees affect snow melt over years
     s(year, event, bs = 'sz', k = 10) +
     # recovery time post event (fire or cut)
@@ -28,26 +44,26 @@ m <- gam(formula = list(
   ~ # mgcv requires one-sided formula for the scale parameter
     ## global changes
     # change in phi over space
-    s(x_alb, y_alb, bs = 'ds', k = 50) +
+    s(x_alb, y_alb, bs = 'ds', k = 25) +
     # seasonal change in phi
-    s(doy, bs = 'cc', k = 10) +
+    #s(doy, bs = 'cc', k = 10) +
     # yearly change in ndvi
     s(year, bs = 'cr', k = 10) +
     # change in seasonal trend over the years
     ti(doy, year, bs = c('cc', 'cr'), k = c(5, 5)) +
     ## treatment-level changes (intercept are included in smooths)
     # trees affect snow melt over doy
-    s(doy, event, bs = 'sz', k = 5, xt = list(bs = 'cc')) +
+    s(doy, event, bs = 'fs', k = 10, xt = list(bs = 'cc')) +
     # trees affect snow melt over years
     s(year, event, bs = 'sz', k = 10) +
     # recovery time post event (fire or cut)
     s(sqrt(years_since), event, bs = 'fs', k = 5)),
   family = betals(), # because data is in [0, 1] range
-  data = d,
+  data = d1,
   method = 'REML', # REstricted Maximum Likelihood
   control = gam.control(trace = TRUE), # print updates while fitting
   knots = list(doy = c(0.5, 366.5))) # for doy to span the full year
-
+toc()
 # ensure the model complexity is reasonable
 plot(m, pages = 1, scheme = 2, scale = 0)
 
